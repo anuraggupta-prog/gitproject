132f41a134421b08e5c67f2ec060b2add8f7759f
anuraggupta-prog <anurag.anurag.4.u@gmail.com>
2021-07-02 18:45:29 +0530

    files commited

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	//testng xml file from maven
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+
+	}
+
+}
