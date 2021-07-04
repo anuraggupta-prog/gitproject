09cde7fd5ac84588432856fbfe12412fc0295d19
anuraggupta-prog <anurag.anurag.4.u@gmail.com>
2021-07-04 11:50:58 +0530

    this is commit from workspace two

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -7,7 +7,11 @@ public class AppiumTest {
 	@Test
 	public void NativeAppAndroid()
 	{
-		System.out.println("NativeAppAndroid");
+		System.out.println("NativeAppAndroid6");
+		System.out.println("NativeAppAndroid7");
+		System.out.println("NativeAppAndroid8");
+		System.out.println("NativeAppAndroid9");
+		System.out.println("NativeAppAndroid10");
 	}
 	
 	@Test
